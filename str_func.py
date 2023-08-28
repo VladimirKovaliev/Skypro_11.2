8d1665e76d035d7d60520d57b1f875e745f78c2f
VladimirKovalev <kovaliev.v@list.ru>
2023-08-28 18:38:28 +0300

    add new func (upper first letter)

--- str_func.py
+++ str_func.py
@@ -1,2 +1,9 @@
 def upper(text):
+    '''Возвращает текст заглавными буквами'''
     return text.upper()
+
+def upper_first_letter(text):
+    '''Делает каждую первую букву всех слов загловными'''
+    words = text.split(' ')
+    upper_first_letter_words = [word.title() for word in words]
+    return ' '.join(upper_first_letter_words)
